fb5caaf3eb705e8de041ccb178c069ba02bcaf49
boostie17 <Aron.Eric.Miljan@tudeng.ut.ee>
2018-09-12 09:24:13 +0300

    Initial commit

---
+++ src/main/java/ut/cs/ee/wsd2018pV/peabVaatama/PeabVaatamaApplication.java
@@ -0,0 +1,12 @@
+package ut.cs.ee.wsd2018pV.peabVaatama;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeabVaatamaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeabVaatamaApplication.class, args);
+	}
+}
---
+++ src/test/java/ut/cs/ee/wsd2018pV/peabVaatama/PeabVaatamaApplicationTests.java
@@ -0,0 +1,16 @@
+package ut.cs.ee.wsd2018pV.peabVaatama;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PeabVaatamaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
