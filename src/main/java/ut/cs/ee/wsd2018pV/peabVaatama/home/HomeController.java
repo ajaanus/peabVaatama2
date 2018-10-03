a83450f60f416e50f97dfe2edaf10fdd396ca548
ajaanus <Jaanus.arukask@tudeng.ut.ee>
2018-10-03 03:04:31 +0300

    Muudatused, et veebileht toimima hakkaks

--- src/main/java/ut/cs/ee/wsd2018pV/peabVaatama/home/HomeController.java
+++ src/main/java/ut/cs/ee/wsd2018pV/peabVaatama/home/HomeController.java
@@ -1,15 +1,17 @@
 package ut.cs.ee.wsd2018pV.peabVaatama.home;
 //src/main/java/ee/ut/cs/wad2018/fall/springbootdemo/home/HomeController.java
 //package ee.ut.cs.wad2018.fall.springbootdemo.home;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+
 @Controller
 public class HomeController {
-    @RequestMapping(path = "/test", method = RequestMethod.GET)
+    @RequestMapping(path = "/midaiganes", method = RequestMethod.GET)
     public String getTestPage() {
         return "home/index";
     }
